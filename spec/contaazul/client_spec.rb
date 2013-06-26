80e37e75537013c35c7bb423f60b73ce935f3d53
João Netto <hi@joaonetto.me>
2013-06-26 13:24:27 -0300

    Fixed request stubs and their tests

--- spec/contaazul/client_spec.rb
+++ spec/contaazul/client_spec.rb
@@ -1,5 +1,6 @@
 # -*- encoding: utf-8 -*-
 require 'spec_helper'
+require 'json'
 
 describe Contaazul::Client do
 
@@ -13,9 +14,9 @@ describe Contaazul::Client do
       Contaazul.reset
     end
 
-    it "defaults to http://api.contaazul.com.br/pub/" do
+    it "defaults to http://app.contaazul.com.br/" do
       client = Contaazul::Client.new
-      expect(client.api_endpoint).to eq('http://api.contaazul.com.br/pub/')
+      expect(client.api_endpoint).to eq('http://app.contaazul.com.br/')
     end
 
     it "is set " do
@@ -27,33 +28,6 @@ describe Contaazul::Client do
   end
 
   context "when working with oauth tokens" do
-    # raise @client
-      # https://api.contaazul.com.br/pub/oauth/requestkey/SUA_CHAVE
-      # to get the public token
-
-      # so, call auth page popup as:
-      # CALLBACK_URL => your callback page url, with http://
-      # PUBLIC_TOKEN => public token from previous step
-
-      # <a href="javascript:window.open(
-      # 'http://app.contaazul.com.br/authorization/request/PUBLIC_TOKEN?
-      # redirectTo=CALLBACK_URL',
-      # 'popUpWindow',
-      # 'toolbar=no,location=no,status=yes,menubar=no,scrollbars=yes,resizable=yes,width=4
-      # 30,
-      # height=500')">
-      # Desejo Integrar com o ContaAzul</a>
-
-      # redirectTo é a URL de retorno para o Aplicativo XYZ. Para essa URL será passado um
-      # parâmetro com a CompanyToken caso o usuário autentique. O parâmetro de retorno da
-      # companyKey é authorizationToken. Exemplo:
-      # http://www.aplicativoxyz.com?auhorizationToken=0000013e-7b7b-bbd6-0000-
-      # 012a9b7962ba
-      # A token 0000013e-7b50-5649-0000-007145a69e35 é única, uma vez chamada ela é
-      # descartada, ou seja, caso seja aberto o popup e usuário fechar, deverá ser criada outra public
-      # token. Esse procedimento garante a segurança das informações no caso de haver um sniffer
-      # na rede, em outras palavras, caso alguém consiga interceptar esse link e tentar se conectar
-      # pela segunda vez, não será possível, pois o token já foi descartado.
 
     before(:each) do
       Contaazul.reset
@@ -61,30 +35,25 @@ describe Contaazul::Client do
     end
 
     it "should request and return a public token" do
-      stub_get("oauth/requestkey/#{Contaazul.external_token}").
-        to_return \
-          :status => 200,
-          :body => "1f74b13f1e5c6c69cb5d7fbaabb1e2cb",
-          :header => {
-            :content_type => 'application/json; charset=utf-8'
-          }
+      stub_request(:get, "http://app.contaazul.com.br/oauth/requestkey/b6445aa301aaff61e641a0b85f26749e").
+         with(:headers => {'Accept'=>'application/json', 'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3', 'User-Agent'=>'Contaazul Non-Official Ruby Gem 0.1.0dev'}).
+         to_return(:status => 200, :body => '{ "publicToken" : "1f74b13f1e5c6c69cb5d7fbaabb1e2cb" }', :headers => {
+          :content_type => "application/json; charset=utf-8"
+          })
 
-      response = Contaazul.get "oauth/requestkey/#{Contaazul.external_token}"
-      expect(response).to eq("1f74b13f1e5c6c69cb5d7fbaabb1e2cb")
+      response = Contaazul.get("oauth/requestkey/#{Contaazul.external_token}")
+      expect(response["publicToken"]).to eq("1f74b13f1e5c6c69cb5d7fbaabb1e2cb")
     end
 
     it "should get a valid oauth2 instance" do
-      stub_get("oauth/requestkey/#{Contaazul.external_token}").
-        to_return \
-          :status => 200,
-          :body => "1f74b13f1e5c6c69cb5d7fbaabb1e2cb",
-          :header => {
-            :content_type => 'application/json; charset=utf-8'
-          }
+      stub_request(:get, "http://app.contaazul.com.br/oauth/requestkey/b6445aa301aaff61e641a0b85f26749e").
+         with(:headers => {'Accept'=>'application/json', 'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3', 'User-Agent'=>'Contaazul Non-Official Ruby Gem 0.1.0dev'}).
+         to_return(:status => 200, :body => '{ "publicToken" : "1f74b13f1e5c6c69cb5d7fbaabb1e2cb" }', :headers => {
+          :content_type => "application/json; charset=utf-8"
+          })
 
       response = Contaazul.get "oauth/requestkey/#{Contaazul.external_token}"
-
-      client = Contaazul::Client.new(:oauth_token => response)
+      # client = Contaazul::Client.new(:oauth_token => response)
     end
 
   end
